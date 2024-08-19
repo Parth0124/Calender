49b3e00f8edd03ebc31c050855671dedb58dd83b
Abhiraj412 <abhangparth@gmail.com>
2024-08-19 15:04:52 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ const App = () => {
   return (
     <div className="container">
       <CalendarApp />
-      <h1 className='port'  >Visit my portfolio <a target="_blank" rel="noreferrer" href='https://shashipreetham.netlify.app/'>click here</a></h1>
     </div>
   )
 }
